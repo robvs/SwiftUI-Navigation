9dc6deb6f83a75771051c1548e395f4db50f8674
Rob Vander Sloot <rob@randomvisual.com>
2022-02-06 23:38:00 -0500

    Get the insertion/removal transition working (mostly) on the root views.

--- SwiftUINavigation/SwiftUINavigation/RootView.swift
+++ SwiftUINavigation/SwiftUINavigation/RootView.swift
@@ -12,18 +12,20 @@ struct RootView: View {
     @StateObject var navigationController = NavigationController()
 
     var body: some View {
-        Group {
-            switch navigationController.visibleRootView {
-            case .mainTabbedView:
-                MainTabbedView(navigationController: navigationController)
+        ZStack {
+            Group {
+                switch navigationController.visibleRootView {
+                case .mainTabbedView:
+                    MainTabbedView(navigationController: navigationController)
 
-            case .baseNavigationView:
-                FullScreenBaseNavigationView(navigationController: navigationController)
+                case .baseNavigationView:
+                    FullScreenBaseNavigationView(navigationController: navigationController)
+                }
             }
+            .animation(.default, value: navigationController.visibleRootView)
+            .transition(AnyTransition.asymmetric(insertion: .move(edge: .trailing),
+                                                 removal: .move(edge: .leading)))
         }
-        .animation(.default, value: navigationController.visibleRootView)
-        .transition(AnyTransition.asymmetric(insertion: .move(edge: .leading),
-                                             removal: .move(edge: .trailing)))
     }
 }
 
